b07315197128b490ae0d012d3a323aa1c9dd62e2
thinker-github01 <thinker.github01@gmail.com>
2023-07-13 16:15:20 +0900

    Add Service User, Schema, Grants

---
+++ terraform/environments/common /provider.tf
@@ -0,0 +1,17 @@
+# terraform {
+#   required_providers {
+#     snowflake = {
+#       source  = "Snowflake-Labs/snowflake"
+#       version = "~> 0.61"
+#     }
+#   }
+# }
+
+# provider "snowflake" {
+#   alias    = "account_provider"
+#   account  = var.account
+#   username = var.username
+#   password = var.password
+#   role     = var.role
+#   region   = var.region
+# }
---
+++ terraform/environments/common /variables.tf
@@ -0,0 +1,19 @@
+# variable "account" {
+#   description = "Snowflake account name"
+# }
+
+# variable "username" {
+#   description = "Snowflake user name"
+# }
+
+# variable "password" {
+#   description = "Snowflake password"
+# }
+
+# variable "role" {
+#   description = "Snowflake role"
+# }
+
+# variable "region" {
+#   description = "Snowflake region"
+# }
