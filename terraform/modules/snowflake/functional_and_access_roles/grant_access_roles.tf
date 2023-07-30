933c2169adde0a19304fc38ab6194f346e19187b
Nok-Y <yhtnok@gmail.com>
2023-07-30 23:14:34 +0900

    update grant future view

--- terraform/modules/snowflake/functional_and_access_roles/grant_access_roles.tf
+++ terraform/modules/snowflake/functional_and_access_roles/grant_access_roles.tf
@@ -83,6 +83,27 @@ resource "snowflake_grant_privileges_to_role" "on_table" {
 }
 
 # on view
+resource "snowflake_grant_privileges_to_role" "on_view" {
+  for_each = {
+    for grant in var.grant_on_object_to_access_role : grant.name => {
+      roles         = grant.role
+      database_name = grant.parameter.database_name
+      schema_name   = grant.parameter.schema_name
+      privileges    = grant.parameter.privileges
+    }
+    if grant.type == "VIEW"
+  }
+  privileges  = each.value.privileges
+  role_name   = "${terraform.workspace}_${each.value.roles}"
+  on_schema_object{
+    future{
+      object_type_plural = "VIEWS"
+      in_schema          = upper("${each.value.database_name}.${terraform.workspace}_${each.value.schema_name}")
+    }
+  }
+  # 先に存在しなければならないが、参照していないので依存関係を付ける
+  depends_on = [snowflake_role.roles, snowflake_database.databases, snowflake_schema.schemas]
+}
 
 # on warehouse
 resource "snowflake_grant_privileges_to_role" "on_warehouse_grant" {
