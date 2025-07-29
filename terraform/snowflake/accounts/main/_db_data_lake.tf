2f1cc133b1fa345ecc1901bff1ec2066ad7add86
Byungsoo.lee <workout4050@gmail.com>
2025-07-29 04:32:07 +0000

    SR_TABLEAUロールにDATA_LAKEデータベースとSERVICE_Aスキーマへの権限付与

--- terraform/snowflake/accounts/main/_db_data_lake.tf
+++ terraform/snowflake/accounts/main/_db_data_lake.tf
@@ -21,7 +21,8 @@ module "data_lake_db" {
   ]
 
   read_only_ar_to_fr_set = [
-    module.fr_analyst.name
+    module.fr_analyst.name,
+    module.sr_tableau.name
   ]
 
   sr_import_ar_to_fr_set = [
@@ -59,7 +60,8 @@ module "data_lake_db_service_a_schema" {
   read_only_ar_to_fr_set = [
     module.fr_data_engineer.name,
     module.fr_scientist.name,
-    module.fr_analyst.name
+    module.fr_analyst.name,
+    module.sr_tableau.name
   ]
 
   sr_import_ar_to_fr_set = [
