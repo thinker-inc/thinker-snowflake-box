031ada3b05517171ac0efea46669a35c4fc4ee18
Byungsoo.lee <workout4050@gmail.com>
2025-07-31 02:44:25 +0000

    カスタムOAuth統合作成

--- terraform/snowflake/accounts/main/_oauth_integration_for_partner_applications.tf
+++ terraform/snowflake/accounts/main/_oauth_integration_for_partner_applications.tf
@@ -4,7 +4,7 @@
 # https://registry.terraform.io/providers/snowflakedb/snowflake/latest/docs/resources/oauth_integration_for_partner_applications
 # ########################
 
-# Tableau Cloud用OAuth統合
+# Tableau Cloud用OAuth統合（パートナーアプリケーション）
 module "tableau_cloud_oauth_integration" {
   source = "../../modules/oauth_integration_for_partner_applications"
   providers = {
@@ -19,6 +19,32 @@ module "tableau_cloud_oauth_integration" {
   oauth_refresh_token_validity = 432000 # 5日
   oauth_use_secondary_roles    = "NONE"
   oauth_roles_ar_to_fr_set     = ["SR_TABLEAU"]
+}
+
+# Tableau Cloud用カスタムOAuth統合（追加）
+resource "snowflake_oauth_integration_for_custom_clients" "tableau_cloud_custom" {
+  provider = snowflake.fr_security_manager
+
+  name                         = "TABLEAU_CLOUD_CUSTOM_OAUTH"
+  oauth_client_type            = "PUBLIC"
+  oauth_redirect_uri           = "https://us-west-2b.online.tableau.com/auth/oauth_redirect"
+  enabled                      = true
+  oauth_issue_refresh_tokens   = true
+  oauth_refresh_token_validity = 432000
+  oauth_enforce_pkce           = true
+  comment                      = "Custom OAuth for Tableau Cloud"
+}
+
+# カスタム統合の権限付与
+resource "snowflake_grant_privileges_to_account_role" "tableau_custom_oauth_usage" {
+  provider = snowflake.fr_security_manager
+
+  privileges        = ["USAGE"]
+  account_role_name = "SR_TABLEAU"
+  on_account_object {
+    object_type = "INTEGRATION"
+    object_name = snowflake_oauth_integration_for_custom_clients.tableau_cloud_custom.name
+  }
 }
 
 # Tableau Desktop用OAuth統合
